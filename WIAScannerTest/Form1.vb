167af2faa1266de97ec07591993522a50d477f63
Dejan Mauer <dejan@HPPRO-DEV>
2018-03-05 00:46:04 +0100

    Init

--- WIAScannerTest/Form1.vb
+++ WIAScannerTest/Form1.vb
@@ -1,4 +1,9 @@
-﻿Public Class Form1
+﻿''' <summary>
+''' SAMPLE BY DEJAN MAUER
+''' dejan.mauer@gmail.com
+''' </summary>
+
+Public Class Form1
 	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 		Dim scanner As New WIAScanner.Scan()
 		Dim device = scanner.SelectDevice()
